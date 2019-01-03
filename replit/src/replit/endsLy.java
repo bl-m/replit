852a5bb85b436babedb2741b04c184872efac652
jamesmoriarty176 <jamesmoriarty176@gmail.com>
2019-01-02 18:29:38 -0600

    gjhgj

---
+++ replit/src/replit/endsLy.java
@@ -0,0 +1,13 @@
+package replit;
+
+public class endsLy {
+	
+	public static void main(String[] args) {
+		
+		String s = "baly";
+		
+		String str = s.charAt(s.length() -2) + "" + s.charAt(s.length()-1);
+		System.out.println(str);
+	}
+
+}
